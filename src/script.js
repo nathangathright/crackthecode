fce127a1b3b57bbece7a72b48849c1b630d66c08
Nathan Gathright <nathan.gathright@gmail.com>
2024-04-07 12:30:30 -0500

    Misc
    • Center the heading
    • Wider max width
    • flexible input
    • fix message text color

--- src/script.js
+++ src/script.js
@@ -56,20 +56,21 @@ messages.forEach((message) => {
     "rounded-3xl",
     "px-4",
     "py-2",
-    "text-black",
-    "dark:text-white",
   );
   if (message.length === 4) {
     bubble.classList.add(
       "bg-blue-500",
       "text-white",
+      "dark:text-white",
       "self-end",
       "rounded-br-1",
     );
   } else {
     bubble.classList.add(
-      "bg-slate-300",
-      "dark:bg-slate-700",
+      "bg-slate-200",
+      "dark:bg-slate-800",
+      "text-black",
+      "dark:text-white",
       "text-slate-900",
       "self-start",
       "rounded-bl-1",
