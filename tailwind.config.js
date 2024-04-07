ac0b11e4e3f083a74367fd0b4744e72267d8b2ab
Nathan Gathright <nathan.gathright@gmail.com>
2024-04-07 16:02:42 -0500

    Shrink keyboard on small screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  theme: {
+    extend: {
+      screens: {
+        short: { raw: "(max-height: 720px)" },
+      },
+    },
+  },
 };
