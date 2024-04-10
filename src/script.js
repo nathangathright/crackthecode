2b92767be646bed2f6a819b0e03796b30ccbc2be
Nathan Gathright <nathan.gathright@gmail.com>
2024-04-10 01:00:25 -0500

    Working Combo logic

--- src/script.js
+++ src/script.js
@@ -15,10 +15,21 @@ let acceptableDigits = {
   3: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
 };
 
+let combinations = [];
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    for (let k = 0; k < 10; k++) {
+      for (let l = 0; l < 10; l++) {
+        combinations.push(`${i}${j}${k}${l}`);
+      }
+    }
+  }
+}
+
 let messages = [
   "Can you crack the code? After each 4-digit guess, I’ll tell you how many digits are in the correct position.",
-  "As you enter each digit, the numeric keyboard will update to show you which digits are still available.",
-  "The logic to update the keyboard is still a work in progress. Good luck!",
+  "As you enter each digit, the numeric keyboard will update to show only the digits that are still possible.",
+  "Good luck!",
 ];
 
 const urlParams = new URLSearchParams(window.location.search);
@@ -40,7 +51,7 @@ const hasFourDigits = () => code.value.length === 4;
 
 const appendMessage = (message, isUser) => {
   const bubble = document.createElement("li");
-  bubble.textContent = message;
+  bubble.innerHTML = message;
   bubble.classList.add(
     "relative",
     "max-w-[85%]",
@@ -76,47 +87,116 @@ messages.forEach((message) => {
   appendMessage(message, false);
 })
 
-const updateAcceptableDigits = () => {
-  guesses.forEach((guess) => {
-    switch (numCorrectDigits(guess)) {
-      case 0:
-        guess.split("").forEach((digit, index) => {
-          acceptableDigits[index] = acceptableDigits[index].filter(
-            (acceptableDigit) => acceptableDigit !== parseInt(digit),
-          );
-        });
-        break;
-      case 1:
-        // determine which digits the user should be able to use
-        break;
-      // Add more cases as needed
-    }
+const updateCombinations = (guess) => {
+  // remove this guess from the list of combinations
+  combinations = combinations.filter((combo) => {
+    return combo !== guess;
+  });
+
+  let [a, b, c, d] = guess.split("").map((digit) => parseInt(digit));
+  switch (numCorrectDigits(guess)) {
+    case 3:
+      // only keep combinations that have exactly 3 of the digits from this guess in the same positions
+      combinations = combinations.filter((combo) => {
+        let [w, x, y, z] = combo.split("").map((digit) => parseInt(digit));
+        return (
+          a === w && b === x && c === y && d !== z ||
+          a === w && b === x && c !== y && d === z ||
+          a === w && c === y && b !== x && d === z ||
+          b === x && c === y && a !== w && d === z
+        );
+      });
+      break;
+    case 2:
+      // only keep combinations that have exactly 2 of the digits from this guess in the same positions
+      combinations = combinations.filter((combo) => {
+        let [w, x, y, z] = combo.split("").map((digit) => parseInt(digit));
+        return (
+          a === w && b === x && c !== y && d !== z ||
+          a === w && b !== x && c === y && d !== z ||
+          a === w && b !== x && c !== y && d === z ||
+          a !== w && b === x && c === y && d !== z ||
+          a !== w && b === x && c !== y && d === z ||
+          a !== w && b !== x && c === y && d === z
+        );
+      });
+      break;
+    case 1:
+      // only keep combinations that have exactly 1 of the digits from this guess in the same positions
+      combinations = combinations.filter((combo) => {
+        let [w, x, y, z] = combo.split("").map((digit) => parseInt(digit));
+        return (
+          a === w && b !== x && c !== y && d !== z ||
+          a !== w && b === x && c !== y && d !== z ||
+          a !== w && b !== x && c === y && d !== z ||
+          a !== w && b !== x && c !== y && d === z
+        );
+      });
+      break;
+    case 0:
+      // only keey combinations that have none of the digits from this guess in the same positions
+      combinations = combinations.filter((combo) => {
+        let [w, x, y, z] = combo.split("").map((digit) => parseInt(digit));
+        return (
+          a !== w &&
+          b !== x &&
+          c !== y &&
+          d !== z
+        );
+      });
+      break;
+  }
+}
+
+const updateAcceptableDigits = (partialguess) => {
+  acceptableDigits = {
+    0: [],
+    1: [],
+    2: [],
+    3: [],
+  };
+
+  combinations.filter((combo) => combo.startsWith(partialguess)).forEach((combo) => {
+    combo.split("").forEach((digit, index) => {
+      if (!acceptableDigits[index].includes(parseInt(digit))) {
+        acceptableDigits[index].push(parseInt(digit));
+      }
+    });
   });
 }
 
 const updateKeyboard = () => {
-  updateAcceptableDigits();
   const inputLength = code.value.length || 0;
   keyboardButtons.forEach((button) => {
     button.disabled = false;
     submitButton.disabled = true;
-    if (isVictorious) button.disabled = true;
     if (inputLength === 4) {
       button.disabled = true;
       submitButton.disabled = false;
       return;
     }
-    if (inputLength === 3) {
-      const guess = code.value + button.value;
-      if (guessesArray.includes(guess)) {
-        button.disabled = true;
-      }
-    }
+    updateAcceptableDigits(code.value);
     if (!acceptableDigits[inputLength].includes(parseInt(button.value))) {
       button.disabled = true;
     }
   });
 };
+
+const generateEmoji = () => {
+  let emoji = "";
+  guesses.forEach((guess) => {
+    emoji += "<br>";
+    guess.split("").forEach((digit, index) => {
+      if (digit === secretCode[index]) {
+        emoji += "🔑";
+      } else {
+        emoji += "🔒";
+      }
+    }
+    );
+  });
+  return emoji;
+}
 
 keyboardButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -131,6 +211,7 @@ document.addEventListener("keydown", (event) => {
 
   if (event.key >= 0 && event.key <= 9) {
     if (hasFourDigits()) return;
+    if (document.querySelector(`#key${event.key}`).disabled) return;
     code.value += event.key;
     updateKeyboard();
   }
@@ -154,17 +235,20 @@ submitButton.addEventListener("click", () => {
   const guess = code.value;
   guesses.push(guess);
   appendMessage(guess, true);
+  updateCombinations(guess);
   code.value = "";
 
   const correctDigits = numCorrectDigits(guess);
   if (correctDigits === 4) {
     isVictorious = true;
     let pluralCount = guesses.length === 1 ? `${guesses.length} guess` : `${guesses.length} guesses`;
-    appendMessage(`Congratulations! You’ve cracked the code! 🎉 It took you ${pluralCount}.`,false);
+    appendMessage(`You cracked the code in ${pluralCount}!${generateEmoji()}`,false);
     appendMessage("Tap the 🔄 button to play again. Tap the 🔀 button to create your own puzzle.", false);
   } else {
-    let plural = correctDigits === 1 ? "" : "s";
-    appendMessage(`${correctDigits} correct digit${plural}`, false);
+    let count = combinations.length;
+    let digits = correctDigits === 1 ? "digit" : "digits";
+    let combos = count === 1 ? "combo" : "combos";
+    appendMessage(`${correctDigits} correct ${digits}! ${count} ${combos} remaining.`, false);
     updateKeyboard();
   }
 });
